4812e02028d528ba4b08c876558a0c32502ed7d8
eyakub <eyakubsorkar@gmail.com>
2020-10-01 21:25:40 +0600

    update and create contacts info done

--- hub_test/urls.py
+++ hub_test/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from hub_test.views import index, individual_data
+from hub_test.views import index, individual_data, create_contact
 
 
 app_name = 'hub_test'
 
 urlpatterns = [
     path('', index, name='index'),
-    path('user_info/', individual_data, name='individual-data')
+    path('user_info/', individual_data, name='individual-data'),
+    path('create_contact', create_contact, name='create-contact'),
 ]
--- hub_test/views.py
+++ hub_test/views.py
@@ -2,6 +2,9 @@ import requests
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.template.response import TemplateResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
@@ -27,25 +30,75 @@ def individual_data(request):
     contact_data = "https://api.hubspot.com/contacts/v1/contact/email/{}/profile?hapikey={}".format(
         email, settings.APP_API_KEY_TEST
     )
+    contact_data_response = requests.get(contact_data)
+    contact_data_response = contact_data_response.json()
+    favorite_book = contact_data_response
+    
+    headers = {}
+    headers['Content-Type']= 'application/json'
+    # return JsonResponse(favorite_book)
+
     try:
         if request.method == 'POST':
-            propUpdate = {
+            
+            data = json.dumps({
                 "properties": [
                     {
                         "property": "favorite_book",
-                        "value": request.body.new_val
+                        "value": request.POST.get('new_val')
                     }
                 ]
-            }
-            apiCall = "https://api.hubspot.com/contacts/v1/contact/createOrUpdate/email/{}/profile?hapikey={}".format(
+            })
+            print('----------prop update---->', data)
+            apiCall = "https://api.hubspot.com/contacts/v1/contact/createOrUpdate/email/{}/?hapikey={}".format(
                 email, settings.APP_API_KEY_TEST
             )
             try:
-                update_response = requests.post(apiCall, propUpdate)
+                update_response = requests.post(url=apiCall, headers=headers, data=data)
+                print('------->', update_response.__dict__)
             except:
                 pass
             return JsonResponse(update_response)
     except:
         pass
+    context = {
+        'favorite_book': favorite_book['properties']['favorite_book']
+    }
+    return TemplateResponse(request, 'hub_test/update_info.html', context)
+
+
+@csrf_exempt
+def create_contact(request):
     context = {}
-    return HttpResponse('hub_test/update_info.html', context)
+
+    endpoint = 'https://api.hubapi.com/contacts/v1/contact/?hapikey={}'.format(settings.APP_API_KEY_TEST)
+    headers = {}
+    headers["Content-Type"]="application/json"
+    
+    if request.method == 'POST':
+        try:
+            data = json.dumps({
+                'properties': [
+                    {
+                        "property": "email",
+                        "value": request.POST.get('email')
+                    },
+                    {
+                        "property": "firstname",
+                        "value": request.POST.get('first_name')
+                    },
+                    {
+                        "property": "lastname",
+                        "value": request.POST.get('last_name')
+                    },
+                    # {
+                    #     "property": "company",
+                    #     "value": "CodedBuzz"
+                    # }
+                ]
+            })
+            create_response = requests.post(url=endpoint, data=data, headers=headers)
+            print(create_response.json())
+        except:
+            pass
+    return TemplateResponse(request, 'hub_test/create_contact.html', context)
